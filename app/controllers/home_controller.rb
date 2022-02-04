b34de556ed9772560c633ebf5fbfc86b2571b57c
sasharevzin <sasha.nike@gmail.com>
2022-02-04 11:08:41 -0500

    Init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    data
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "the truth" do
+    # Parallel.each((1..20).to_a) do
+    #   get "/"
+    # end
+  end
+end
